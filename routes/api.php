63ef0004114c3548ed64b22c9f67cf42f7b0dc8d
Gren <eeli.gren@fimes.fi>
2024-05-20 17:29:37 +0300

    API db test

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/test', function (Request $request) {
+    $users = \App\Models\User::all();
+
+    return response()->json($users);
+});
